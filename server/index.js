49215c02bd2f89d15b06395daed0cd6faf3ee7ea
SNEHA CHAUHAN <sneha2003chauhan@gmail.com>
2023-07-31 02:58:40 +0530

    Changes in the files

--- server/index.js
+++ server/index.js
@@ -5,13 +5,25 @@ const socketIO = require('socket.io');
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
 const server = http.createServer(app);
 
 const io = socketIO(server);
 
 const port = 4500 || process.env.PORT ;
 
+io.on('connection', (socket) => {
+    console.log('New client connected');
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
+});
+
+
 server.listen(port, () => {
-    console.log('Server started!');
+    console.log(`Server started on http://localhost:${port}`);
     }
 );
